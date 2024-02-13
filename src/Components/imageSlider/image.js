e7e4e14f653d7a3cc0a0d6c16c6b83cc7e276a04
Jayanta Roy <122526659+iamjayantaroy@users.noreply.github.com>
2024-02-13 17:15:58 +0530

    Delete src/Components/imageSlider directory

--- src/Components/imageSlider/image.js
+++
@@ -1,9 +0,0 @@
-const slides = [
-  { url: "http://localhost/5173/image-1.jpg", title: "Beach" },
-  { url: "http://localhost/5173/image-2.jpg", title: "Boat" },
-  { url: "http://localhost/5173/image-3.jpg", title: "forest" },
-  { url: "http://localhost/5173/image-4.jpg", title: "City" },
-  { url: "http://localhost/5173/image-5.jpg", title: "Italy" },
-];
-
-export default slides;
